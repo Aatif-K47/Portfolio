0df3df5d8518b2dcc5e1580b3cecd5b13aaeb5d9
AmiGo0o <aatifkhanpk98@gmail.com>
2025-02-24 10:13:34 +0500

    first commit

---
+++ components/about.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+  After completing my studies, I discovered a passion for{" "}
+  <span className="font-medium">web development</span> and decided to dive into{" "}
+  <span className="font-medium">frontend programming</span>. I started with 
+  HTML, CSS, and JavaScript, and quickly fell in love with{" "}
+  <span className="font-medium">React</span>.{" "}
+  <span className="italic">My favorite part of programming</span> is 
+  problem-solving—I <span className="underline">love</span> breaking down 
+  complex challenges and finding efficient solutions. My core stack includes{" "}
+  <span className="font-medium">React, Next.js, and Redux</span>. I am always 
+  eager to learn new technologies and improve my skills. I am currently looking 
+  for a <span className="font-medium">frontend developer</span> role where I can 
+  contribute my skills and grow as a developer.
+</p>
+
+<p>
+  <span className="italic">When I'm not coding</span>, I am a{" "}
+  <span className="font-medium">sports enthusiast</span>. I love staying active, 
+  whether it's playing <span className="font-medium">cricket, football, Badminton</span>, 
+  or hitting the gym. Sports keep me disciplined, motivated, and always striving 
+  for improvement—just like in programming!
+</p>
+
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,111 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Web Developer intern | Ford9 Solutions",
+    location: "Mingora, KPK, Pakistan",
+    description:
+      "Gained hands-on experience with HTML, CSS, and JavaScript, building responsive web pages and improving UI/UX designs. Worked closely with senior developers, learning best practices in clean code, performance optimization, and accessibility. This role laid the foundation for frontend development skills and introduced me to React.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan 2024 - Mar 2024",
+  },
+  {
+    title: "React Developer Intern | CyberVision international",
+    location: "Mingora, KPK, Pakistan",
+    description:
+      "Focused on React development, working on dynamic UI components and integrating APIs. Improved understanding of React hooks, state management, and reusable components. Collaborated with the team to debug issues and optimize application performance, strengthening problem-solving skills.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jun 2024 - Jul 2024",
+  },
+  {
+    title: "Frontend Developer Intern | Evamp & Saanga",
+    location: "Islamabad, Pakistan",
+    description:
+      "Deeply involved in building scalable React applications with Redux for state management. Worked on real-world projects, including an e-commerce platform with an admin dashboard, implementing CRUD operations, authentication, and responsive design. This role has enhanced the ability to write clean, maintainable code, collaborate in an agile team, and handle complex frontend challenges.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Oct 2024 - Feb 2025",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: corpcommentImg,
+    link: "https://corpcomment.com",
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+    link: "https://rmtdev.com",
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+    link: "https://wordanalytics.com",
+  },
+] as const;
+
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
